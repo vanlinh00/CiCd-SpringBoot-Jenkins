c0e6473d9aa66b0b3b4922302115db6a24932734
vanlinh00 <vanlinh20192019@gmail.com>
2025-06-11 10:59:52 +0700

    no message

---
+++ src/main/java/com/example/SpringBootTestCiCd/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.SpringBootTestCiCd.controller;
+
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/home")
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<?> register() {
+
+        return ResponseEntity.ok("User registered");
+    }
+
+}
