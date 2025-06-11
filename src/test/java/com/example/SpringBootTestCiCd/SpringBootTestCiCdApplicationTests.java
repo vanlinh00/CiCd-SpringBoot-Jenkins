2a2715e3b71e76d1fa79d38ec986b73a83949fbb
vanlinh00 <vanlinh20192019@gmail.com>
2025-06-11 10:40:26 +0700

    first commit

---
+++ src/test/java/com/example/SpringBootTestCiCd/SpringBootTestCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootTestCiCd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
